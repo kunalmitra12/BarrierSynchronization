98a70973f4125a0c50aea60812d3789c142d7104
Kunal Mitra <kmitra6@gatech.edu>
2021-10-24 18:36:41 -0400

    harness test for combined

--- combined/harness.c
+++ combined/harness.c
@@ -6,6 +6,8 @@
 #include "gtmp.h"
 #include "gtmpi.h"
 
+#define SECTOUSEC 1000000
+
 int main(int argc, char** argv)
 {
     int num_threads, num_iter=10;
@@ -17,6 +19,9 @@ int main(int argc, char** argv)
     num_processes = strtol(argv[1], NULL, 10);
     num_iter = strtol(argv[3], NULL, 10);
 
+    double time; // store experiment results
+    double threads_sum = 0;
+
     omp_set_dynamic(0);
     if (omp_get_dynamic())
     printf("Warning: dynamic adjustment of threads has been set\n");
@@ -26,19 +31,29 @@ int main(int argc, char** argv)
     gtmp_init(num_threads);
     gtmpi_init(num_processes);
 
+    long delta_total;
+    struct timeval start, end; // to record times
+    int thread_num = omp_get_thread_num();
+    int i;
+
     //combined_init(num_threads, num_processes);
-    for(int i=0; i < num_iter; ++i)
+    gettimeofday(&start, NULL);
+    for(i=0; i < num_iter; ++i)
     {
         #pragma omp parallel shared(num_threads)
         {
             gtmp_barrier();
         }
-
         gtmpi_barrier();
     }
+    gettimeofday(&end, NULL);
+    delta_total = (end.tv_sec * SECTOUSEC + end.tv_usec) - (start.tv_sec * SECTOUSEC + start.tv_usec);
+    time = delta_total / (double)num_iter;
     gtmpi_finalize();
     MPI_Finalize();
     gtmp_finalize();
+
+    printf("Average Time: %f\n", time);
 
     return 0;
 }
